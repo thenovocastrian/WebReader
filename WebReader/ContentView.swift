be0a4179926f06ec50d888880be8926da51461d2
Joel C <thefulldev@proton.me>
2025-05-06 16:23:17 +1000

    Initial Commit

---
+++ WebReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebReader
+//
+//  Created by Joel Caldwell on 6/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebReader/WebReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebReaderApp.swift
+//  WebReader
+//
+//  Created by Joel Caldwell on 6/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct WebReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
